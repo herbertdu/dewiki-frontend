91be30b1a5363578947305328278730fb1d23c4b
herberdu <0xherbertdu@gmail.com>
2024-05-22 12:01:56 +0800

    Editor pin toolbar

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -5,33 +5,41 @@ import { LANG_REGION_MAP } from '../constants/env';
 import 'vditor/dist/index.css';
 
 export interface IVditorEditorProps {
-    keyID: string;
-    options?: IOptions;
-    bindVditor?(vditor: Vditor): void;
-    initialValue?: string;
+  keyID: string;
+  options?: IOptions;
+  bindVditor?(vditor: Vditor): void;
+  initialValue?: string;
 }
 
 const Editor: FC<IVditorEditorProps> = ({ keyID, options, bindVditor, initialValue = '' }) => {
-    const vditorRef = createRef<HTMLDivElement>();
-    const { activeLanguage } = useVoerkaI18n();
+  const vditorRef = createRef<HTMLDivElement>();
+  const { activeLanguage } = useVoerkaI18n();
 
-    useEffect(() => {
-        let vditorLang: keyof II18n = LANG_REGION_MAP[
-            (activeLanguage as keyof typeof LANG_REGION_MAP) || 'en'
-        ] as keyof II18n;
-        const vditor = new Vditor(keyID, {
-            after: () => {
-                vditor.setValue(initialValue);
-                if (!!bindVditor) {
-                    bindVditor(vditor);
-                }
-            },
-            outline: { enable: true, position: 'left' },
-            lang: vditorLang,
-        });
-    }, [activeLanguage]);
+  useEffect(() => {
+    let vditorLang: keyof II18n = LANG_REGION_MAP[
+      (activeLanguage as keyof typeof LANG_REGION_MAP) || 'en'
+    ] as keyof II18n;
+    const vditor = new Vditor(keyID, {
+      after: () => {
+        vditor.setValue(initialValue);
+        if (!!bindVditor) {
+          bindVditor(vditor);
+        }
+      },
+      outline: { enable: true, position: 'left' },
+      lang: vditorLang,
+      counter: { enable: true },
+      toolbarConfig: {
+        hide: false,
+        pin: true,
+      },
+      preview: {
+        actions: ['desktop', 'tablet', 'mobile'],
+      },
+    });
+  }, [activeLanguage]);
 
-    return <div id={keyID} ref={vditorRef}></div>;
+  return <div id={keyID} ref={vditorRef}></div>;
 };
 
 export default Editor;
